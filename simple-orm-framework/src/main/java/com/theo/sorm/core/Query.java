6c9645c9b9daf59cb56b5da9d46979087b9db2d8
黄随心 <993537178@qq.com>
2019-10-23 14:53:41 +0800

    bug fixed

--- simple-orm-framework/src/main/java/com/theo/sorm/core/Query.java
+++ simple-orm-framework/src/main/java/com/theo/sorm/core/Query.java
@@ -231,7 +231,7 @@ public abstract class Query implements Cloneable{
     Object queryUniqueRow(String sql, Class clazz, Object[] params) {
         List list = queryRows(sql, clazz, params);
 
-        return (list == null && list.size() > 0) ? null : list.get(0);
+        return (list == null || list.size() > 0) ? null : list.get(0);
     }
 
     /**
